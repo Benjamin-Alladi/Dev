291ea3bc02f0c2098368edd9cf97a0a184be027e
Benjamin Alladi <147035130+Benjamin-Alladi@users.noreply.github.com>
2023-12-04 15:58:58 +0530

    Paragraph Generator

--- JSProjects/Paragraph Generator/index.js
+++ JSProjects/Paragraph Generator/index.js
@@ -1,69 +1,88 @@
-let wrapper= document.querySelector(".wrapper");
-let inpbox= document.getElementById("inpbox");
+let wrapper = document.querySelector(".wrapper");
+let inpbox = document.getElementById("inpbox");
 
-function getRes(wcount)
-{
-    let p="";
-    let a= [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+let count = 0;
 
-    for(let i=0; i<wcount; i++)
-    {
-        let word="";
-        let wlen= Math.random(1)*10;
-        wlen= Math.ceil(wlen);
-        console.log("wlen: "+wlen);
+function getRes(wcount) {
+    let p = "";
+    let a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-        for(let j=0; j<wlen; j++)
-        {
-            let ind= Math.floor(Math.random()*25);
-            word+= a[ind];
+    for (let i = 0; i < wcount; i++) {
+        let word = "";
+        let wlen = Math.random(1) * 10;
+        wlen = Math.ceil(wlen);
+        // console.log("wlen: "+wlen);
+
+        for (let j = 0; j < wlen; j++) {
+            let ind = Math.floor(Math.random() * 25);
+            word += a[ind];
         }
-        p+= word+" ";
+        p += word + " ";
     }
 
     return p;
 }
 
-function genPara()
-{
-    let ele= document.createElement("p");
+function genPara() {
+    let ele = document.createElement("p");
     ele.classList.add("pg");
 
-    let wcount= inpbox.value;
-    console.log(wcount +" "+ typeof wcount);
-    if(wcount.length==0 || parseInt(wcount)<=0)
-    {
+    let wcount = inpbox.value;
+    console.log(wcount + " " + typeof wcount);
+    if (wcount.length == 0 || parseInt(wcount) <= 0) {
         console.log("In if block.");
         alert("Number of words in the paragraph must be greater than 0.");
         return;
     }
-    else
-    {
+    else {
         console.log("In else-block.");
-        let res= getRes(wcount);
-    
+        let res = getRes(wcount);
+
+        let cl = `pg${count}`;
+        ele.classList.add(cl);
+        count++;
+
         ele.append(res);
         wrapper.append(ele);
 
-        let b= document.createElement("button");
-        b.classList.add("clrbtn");
-        b.innerHTML="Clear";
-        ele.insertAdjacentElement("afterend",b);
+        let btn = document.createElement("button");
+        btn.classList.add("clrbtn", cl);
+        btn.innerHTML = "Clear";
+
+        btn.classList.add(cl);
+
+        ele.insertAdjacentElement("afterend", btn);
+
+        // console.log(ele.classList+" "+ btn.classList);
     }
-    
+
+};
+
+
+let buttons = document.getElementsByClassName("clrbtn");
+
+/*
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(event) { clr(event);});
 }
+*/
+wrapper.addEventListener("click", function(event) {
+    if (event.target.classList.contains("clrbtn")) {
+        clr(event);
+    }
+});
 
-// EventListener on clrbtn:
-let clrbtn= document.getElementsByClassName("clrbtn");
+function clr(event) {
+    console.log("el");
+    let targetClass = event.target.classList[1];
 
-clrbtn.addEventListener("click", clr(this));
+    console.log(targetClass);
 
-function clr(ele)
-{
-    let pnode= ele.parentNode;
-    console.log("Parent Node: "+ pnode);
+    let target = document.getElementsByClassName(targetClass);
+    console.log(target);
 
-    let cnodes= pnode.childNodes;
-    // p.removeChild()
-    console.log(cnodes+" "+ typeof cnodes);
+    for (let i = 0; i < target.length; i++) {
+        event.target.remove();
+        target[i].remove();
+    }
 }
