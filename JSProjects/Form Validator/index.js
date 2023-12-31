6ca91ca106466f36552881e2f2e39ce1d2e6a521
Benjamin Alladi <147035130+Benjamin-Alladi@users.noreply.github.com>
2023-12-31 17:48:04 +0530

    JS Added in FV

--- JSProjects/Form Validator/index.js
+++ JSProjects/Form Validator/index.js
@@ -1,11 +1,42 @@
 let fn= document.querySelector("#fullname");
 let num= document.querySelector("#number");
 let em= document.querySelector("#email");
+let pswrd= document.querySelector("#pswrd");
+let msg= document.querySelector("#msg");
 let btn= document.querySelector(".btn");
+
+let popup= document.querySelector(".popup");
+let ppicon= document.querySelector(".ppicon");
+let ppmsg= document.querySelector(".ppmsg");
+let ppbtn= document.querySelector(".ppbtn");
+
+ppbtn.addEventListener("click", ()=>{popup.classList.remove("popup-open")});
+
+let eye= document.querySelector("#eye");
+eye.addEventListener("click", pdhandler);
+
+function pdhandler()
+{
+    if(eye.classList.contains("fa-eye-slash"))
+    {
+        pswrd.type= "password";
+        eye.classList.replace("fa-eye-slash", "fa-eye");
+    }
+
+    else if(eye.classList.contains("fa-eye"))
+    {
+        pswrd.type= "text";
+        eye.classList.replace("fa-eye", "fa-eye-slash");
+        // console.log(eye);
+    }
+    
+}
 
 fn.addEventListener("input", checkName);
 num.addEventListener("input", checkMobile);
 em.addEventListener("input", checkMail);
+pswrd.addEventListener("input", checkPassword);
+msg.addEventListener("input", checkMessage);
 
 function checkName()
 {
@@ -57,21 +88,57 @@ function checkMail()
 }
 function checkPassword()
 {
+    let v= (pswrd.value);
+    let sp= document.querySelector("#perror");
 
+    if(v.length<6)
+    {
+        sp.innerHTML= '<i id="red" class="fa fa-exclamation-circle"> Requires atleast 6 characters</i>';
+        return false;
+    }
+    if(!v.match( /[\d]/))
+    {
+        console.log("exe");
+        sp.innerHTML= '<i id="red" class="fa fa-exclamation-circle">A digit is required</i>';
+        return false;
+    }
+    sp.innerHTML= '<i id="green" class="fa fa-check"></i>';
+    return true;
 }
 function checkMessage()
 {
+    let l= msg.value.length;
+    let req= 10-l;
 
-}
+    let sp= document.querySelector("#msgerror");
 
-function check()
-{
-    if(!checkName() || !checkMobile() || !checkMail() || !checkMessage())
+    if(req>0)
     {
+        sp.innerHTML= `<i id="red" class="fa fa-exclamation-circle"> ${req} more characters required </i>`;
         return false;
     }
     else
     {
+        sp.innerHTML= '<i id="green" class="fa fa-check"></i>';
         return true;
+    }
+}
+
+function check()
+{
+    if(!checkName() || !checkMobile() || !checkMail() || !checkPassword() || !checkMessage())
+    {
+        ppicon.outerHTML= '<i id="ppred" class="fa fa-exclamation-circle ppicon">';
+        ppmsg.innerHTML= "Please fill the correct details.";
+        popup.classList.add("popup-open");
+        return false;
+    }
+    else
+    {
+        ppicon.outerHTML= '<i id="ppgreen" class="fa fa-check ppicon"></i>';
+        ppmsg.innerHTML= "Form Submitted Successfully";
+        popup.classList.add("popup-open");
+        
+        setTimeout(()=>{return true}, 20000);
     }
 }
